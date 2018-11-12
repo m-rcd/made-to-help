590a1687671d8ef1e0c05f1079d7b3ca4b7486ad
Alex Kharouk <akharoukpp@gmail.com>
2018-11-12 17:23:21 +0000

    hardcode blue dot onto screen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,6 +1,14 @@
 import React from "react";
-import { MapView } from "expo";
+import { MapView, Marker } from "expo";
 
+const home = {
+  latlng: {
+    latitude: 51.517292,
+    longitude: -0.07327
+  },
+  title: "Makers Academy",
+  description: "Home is where the heart is."
+};
 export default class MapScreen extends React.Component {
   render() {
     return (
@@ -12,7 +20,14 @@ export default class MapScreen extends React.Component {
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421
         }}
-      />
+      >
+        <MapView.Marker
+          coordinate={home.latlng}
+          title={home.title}
+          description={home.desc}
+          image={require("../assets/images/location.png")}
+        />
+      </MapView>
     );
   }
 }
