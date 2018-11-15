4273165047d5bf9aa8c61f19a556f5a2d09354cf
Darcie <darciewalsh@hotmail.com>
2018-11-15 16:31:43 +0000

    Fix linting

--- components/DynamicLocation.js
+++ components/DynamicLocation.js
@@ -5,8 +5,8 @@ import MapViewDirections from 'react-native-maps-directions';
 
 const GEOLOCATION_OPTIONS = { enableHighAccuracy: true };
 
-const origin = {latitude: 37.3318456, longitude: -122.0296002};
-const destination = {latitude: 37.771707, longitude: -122.4053769};
+const origin = { latitude: 37.3318456, longitude: -122.0296002 };
+const destination = { latitude: 37.771707, longitude: -122.4053769 };
 const GOOGLE_MAPS_APIKEY = 'AIzaSyB2rfzTA_qJznxhWxxxpuU4e2e6WvLPklk';
 
 export default class DynamicLocation extends React.Component {
@@ -15,7 +15,6 @@ export default class DynamicLocation extends React.Component {
     location: { coords: { latitude: -0.09, longitude: 51 } },
     journeyDistance: null,
     journeyTime: null,
-    journeyInfo: null
   };
 
   componentWillMount = async () => {
@@ -38,30 +37,35 @@ export default class DynamicLocation extends React.Component {
   render() {
     return (
       <View
-        style={{flex: 1}}>
+        style={{ flex: 1 }}
+      >
         <Text
-          style={{flex: 0.25}}> {
+          style={{ flex: 0.25 }}
+        >
+          {' '}
+          {
             `${this.state.journeyTime} - Time \n ${this.state.journeyDistance} - Distance`
           }
         </Text>
-      <MapView
-        style={{ flex: 0.75 }}
-        showsUserLocation region={this.state.region}
-        provider="google"
+        <MapView
+          style={{ flex: 0.75 }}
+          showsUserLocation
+          region={this.state.region}
+          provider="google"
         >
-        <MapViewDirections
-          origin={origin}
-          destination={destination}
-          apikey={GOOGLE_MAPS_APIKEY}
-          strokeWidth={3}
-          strokeColor="hotpink"
-          mode="walking"
-          onReady={(result) => {
-            this.setState({journeyInfo: result, journeyTime: result.duration, journeyDistance: result.distance})
+          <MapViewDirections
+            origin={origin}
+            destination={destination}
+            apikey={GOOGLE_MAPS_APIKEY}
+            strokeWidth={3}
+            strokeColor="hotpink"
+            mode="walking"
+            onReady={(result) => {
+              this.setState({ journeyTime: result.duration, journeyDistance: result.distance });
             }
           }
-        />
-      </MapView>
+          />
+        </MapView>
       </View>
     );
   }
