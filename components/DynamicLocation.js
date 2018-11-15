621b178f9901116b0ac456794f533d7ebb609bc4
Darcie <darciewalsh@hotmail.com>
2018-11-15 15:55:09 +0000

    Add mapview direction functionality
    - Add polyline between two locations
    - Add journey time and distance text

--- components/DynamicLocation.js
+++ components/DynamicLocation.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { View, Text } from 'react-native';
 import { MapView, Location, Permissions } from 'expo';
 import RouteTracker from './RouteTracker';
+import MapViewDirections from 'react-native-maps-directions';
 
 const GEOLOCATION_OPTIONS = { enableHighAccuracy: true };
+
+const origin = {latitude: 37.3318456, longitude: -122.0296002};
+const destination = {latitude: 37.771707, longitude: -122.4053769};
+const GOOGLE_MAPS_APIKEY = 'AIzaSyB2rfzTA_qJznxhWxxxpuU4e2e6WvLPklk';
 
 export default class DynamicLocation extends React.Component {
   state = {
     /* eslint-disable-next-line */
     location: { coords: { latitude: -0.09, longitude: 51 } },
+    journeyDistance: null,
+    journeyTime: null,
+    journeyInfo: null
   };
 
   componentWillMount = async () => {
@@ -29,7 +38,32 @@ export default class DynamicLocation extends React.Component {
 
   render() {
     return (
-      <MapView style={{ flex: 1 }} showsUserLocation region={this.state.region} provider="google" />
+      <View
+        style={{flex: 1}}>
+        <Text
+          style={{flex: 0.25}}> {
+            `${this.state.journeyTime} - Time \n ${this.state.journeyDistance} - Distance`
+          }
+        </Text>
+      <MapView
+        style={{ flex: 0.75 }}
+        showsUserLocation region={this.state.region}
+        provider="google"
+        >
+        <MapViewDirections
+          origin={origin}
+          destination={destination}
+          apikey={GOOGLE_MAPS_APIKEY}
+          strokeWidth={3}
+          strokeColor="hotpink"
+          mode="walking"
+          onReady={(result) => {
+            this.setState({journeyInfo: result, journeyTime: result.duration, journeyDistance: result.distance})
+            }
+          }
+        />
+      </MapView>
+      </View>
     );
   }
 }
