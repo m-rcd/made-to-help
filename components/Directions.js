236f0000fa92d310188a7de7cacbb919eaa49b02
Marianne Rachid <marianne.rachid@gmail.com>
2018-11-16 22:21:01 +0000

    Update directions
    - Moved GooglePlacesAutocomplete tag from DirectionScreen file to
    directions.js file
    - Removed position: 'absolute' from style in GooglePlacesAutocomplete to
    get current position outside the box
    - save address, longitude and latitude from input

--- components/Directions.js
+++ components/Directions.js
@@ -1,21 +1,79 @@
 import React from 'react';
-import { AppRegistry, TextInput } from 'react-native';
+import { View, Text } from 'react-native';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
 
 export default class Directions extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { text: 'Destination' };
+  state = {
+    address: '',
+    latitude: '',
+    longitude: '',
   }
+  // constructor(props) {
+  //   super(props);
+  //   this.state = { address: '', latitude: '', longitude: '' };
+  // }
 
   render() {
     return (
-      <TextInput
-        style={{ height: 20, borderColour: 'gray', borderWidth: 1 }}
-        inChangeText={text => this.setState({ text })}
-        value={this.state.text}
-      />
+      <View style={{ flex: 1 }}>
+        <Text style={{ flex: 0.10 }}>
+          {`${this.state.address} - ${this.state.longitude} - ${this.state.latitude}`}
+        </Text>
+        <GooglePlacesAutocomplete
+          placeholder="Search"
+          minLength={2}
+          autoFocus={false}
+          returnKeyType="search"
+          listViewDisplayed="auto"
+          fetchDetails
+          renderDescription={row => row.description}
+          onPress={(data, details = null) => {
+            this.setState(
+              {
+                address: data.description,
+                latitude: details.geometry.location.lat,
+                longitude: details.geometry.location.lng,
+              },
+            );
+          }}
+
+          getDefaultValue={() => ''}
+
+          query={{
+            key: 'AIzaSyB2rfzTA_qJznxhWxxxpuU4e2e6WvLPklk',
+            language: 'en',
+          }}
+
+          styles={{
+            textInputContainer: {
+              width: '100%',
+            },
+            description: {
+              fontWeight: 'bold',
+            },
+            predefinedPlacesDescription: {
+              color: '#1faadb',
+            },
+            listView: {
+              color: 'black',
+              zIndex: 16,
+            },
+          }}
+          nearbyPlacesAPI="GooglePlacesSearch"
+          GoogleReverseGeocodingQuery={{
+          }}
+          GooglePlacesSearchQuery={{
+            rankby: 'distance',
+          }}
+
+          filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']}
+          debounce={200}
+          currentLocation
+        />
+      </View>
+
     );
   }
 }
 
-AppRegistry.registerComponent('made-to-help', () => Directions);
+// AppRegistry.registerComponent('made-to-help', () => Directions);
--- screens/DirectionsScreen.js
+++ screens/DirectionsScreen.js
@@ -1,75 +1,21 @@
 import React from 'react';
-// import {
-//   Text, TouchableOpacity, TextInput, View, Image, StyleSheet,
-//
 import { View } from 'react-native';
-import { createStackNavigator } from 'react-navigation';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import MapScreen from './MapScreen';
+import Directions from '../components/Directions';
 
 // eslint-disable-next-line
-const App = createStackNavigator({
-  Home: MapScreen,
-});
+// const App = createStackNavigator({
+//   Home: MapScreen,
+// });
 
-export default class Directions extends React.Component {
+export default class DirectionScreen extends React.Component {
   static navigationOptions = {
     title: 'Directions',
   };
-  // constructor(props) {
-  //   super(props);
-  //   // this.state = { text: 'Destination' };
-  // }
 
   render() {
     return (
-      <View>
-        <GooglePlacesAutocomplete
-          placeholder="Search"
-          minLength={2}
-          autoFocus={false}
-          returnKeyType="search"
-          listViewDisplayed="auto"
-          fetchDetails
-          renderDescription={row => row.description}
-          onPress={(data, details = null) => {
-            console.log(data, details);
-          }}
-
-          getDefaultValue={() => ''}
-
-          query={{
-            key: 'AIzaSyB2rfzTA_qJznxhWxxxpuU4e2e6WvLPklk',
-            language: 'en',
-          }}
-
-          styles={{
-            textInputContainer: {
-              width: '100%',
-            },
-            description: {
-              fontWeight: 'bold',
-            },
-            predefinedPlacesDescription: {
-              color: '#1faadb',
-            },
-            listView: {
-              color: 'black',
-              zIndex: 16,
-              position: 'absolute',
-            },
-          }}
-          nearbyPlacesAPI="GooglePlacesSearch"
-          GoogleReverseGeocodingQuery={{
-          }}
-          GooglePlacesSearchQuery={{
-            rankby: 'distance',
-          }}
-
-          filterReverseGeocodingByTypes={['locality', 'administrative_area_level_3']}
-          debounce={200}
-          currentLocation
-        />
+      <View style={{ flex: 1 }} testID="directions">
+        <Directions />
       </View>
     );
   }
