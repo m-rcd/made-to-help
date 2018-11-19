d8e931b0fef9a5071337fafa8b43d3372b0bd0ef
Alex Kharouk <akharoukpp@gmail.com>
2018-11-19 16:54:06 +0000

    Debug DisplayAlerts

--- components/DisplayAlertMarkers.js
+++ components/DisplayAlertMarkers.js
@@ -1,42 +1,43 @@
 import * as firebase from 'firebase';
 import React from 'react';
 import {
-  View, TextInput, Text, Button, Alert,
+  View, Button,
 } from 'react-native';
-import { Location } from 'expo';
+import { MapView } from 'expo';
 
 export default class DisplayAlertMarkers extends React.Component {
   state = {
-    markers: [],
     isLoading: true,
-  }
+    markers: [],
+  };
 
   getMarkerData = () => {
-    firebase.database().ref('alerts/').on('value', (snapshot) => {
-      let data = snapshot.val();
-      let items = Object.values(data);
-      this.setState({markers: items})
-    });
-  }
+    firebase
+      .database()
+      .ref('alerts/')
+      .on('value', (snapshot) => {
+        const data = snapshot.val();
+        const items = Object.values(data);
+        this.setState({ markers: items });
+      });
+  };
 
   render() {
     return (
-      <Button title="Alerts" onPress={this.getMarkerData} />
-      {this.state.isLoading ? null : this.state.markers.map((marker, index) => {
-        const coords = {
-          latitude: marker.location.latitude,
-          longitude: marker.location.longitude,
-        };
-        const alertData = `${marker.body}`;
+      <View>
+        {this.state.isLoading
+          ? null
+          : this.state.markers.map((marker, index) => {
+            const coords = {
+              latitude: marker.location.latitude,
+              longitude: marker.location.longitude,
+            };
+            const alertData = `${marker.body}`;
 
-        return (
-          <MapView.Marker
-          key={index}
-          coordinate={coords}
-          title={alertData}
-        />
-        );
-      })}
-    )
+            return <MapView.Marker key={index} coordinate={coords} title={alertData} />;
+          })}
+        <Button title="Alerts" onPress={this.getMarkerData} />
+      </View>
+    );
   }
-};
+}
