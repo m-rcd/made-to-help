5128615fd54c5a8395d3a32f1017231e2eb05d72
Emma Albury <emma_albury@hotmail.com>
2018-11-20 17:25:39 +0000

    Add Test for origin and destination components

--- components/Directions.js
+++
@@ -1,3 +0,0 @@
-// import React from 'react';
-// import { View, Stylesheet } from 'react-native';
-// import KEY from '../env.config';
--- components/__tests__/Destination.test.js
+++ components/__tests__/Destination.test.js
@@ -2,37 +2,25 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Origin from '../Origin';
+import Destination from '../Destination';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-test('Renders Origin component', () => {
-  const snapshot = renderer.create(<Origin />).toJSON()
+test('Renders Destination component', () => {
+  const snapshot = renderer.create(<Destination />).toJSON()
   expect(snapshot).toMatchSnapshot();
 })
 
 it('State should start empty', () => {
-  const wrapper = shallow(<Origin />);
+  const wrapper = shallow(<Destination />);
   expect(wrapper.instance().state.latitude).toBe('');
   expect(wrapper.instance().state.longitude).toBe('');
 })
 
 it('Should test for setState changes', () => {
-  const wrapper = shallow(<Origin />);
+  const wrapper = shallow(<Destination />);
   const instance = wrapper.instance()
   instance.savingLocation(-0.09, 51)
   expect(instance.state.latitude).toBe(-0.09)
   expect(instance.state.longitude).toBe(51)
 })
-
-// it('setStates onPress', () => {
-//   const data = 'test'
-//   const details = 'hi'
-//   const onPress = jest.fn();
-//   const component = shallow(<Directions onPress={onPress}/>);
-//   component.find('GooglePlacesAutocomplete').simulate('press', {
-//         target: {  details, data},
-//       });
-//
-//     expect(onPress).toBeCalledWith(details, data);
-// });
